29e253a84fc15adc78e467de6b83eb3a1ffed3aa
“Maria <maria.miska09@gmail.com>
2022-04-23 18:09:31 -0400

    aiuda

--- src/main/java/com/astralTinderV1/repositories/UserRepository.java
+++ src/main/java/com/astralTinderV1/repositories/UserRepository.java
@@ -1,6 +1,7 @@
 package com.astralTinderV1.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Repository;
 
 @Repository
