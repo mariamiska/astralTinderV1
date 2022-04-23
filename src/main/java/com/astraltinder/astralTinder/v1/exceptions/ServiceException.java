e370740535b1c5c85be371c237d9eaa99afa4e51
Carlos Carmona <caneladelmoro@gmail.com>
2022-04-23 18:27:54 -0300

    carpetas

---
+++ src/main/java/com/astraltinder/astralTinder/v1/exceptions/ServiceException.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package com.astraltinder.astralTinder.v1.exceptions;
+
+/**
+ *
+ * @author charl
+ */
+public class ServiceException {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+    }
+}
