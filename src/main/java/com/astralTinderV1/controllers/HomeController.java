0544203527739a91b644d2269909f033ab665180
Carlos Carmona <caneladelmoro@gmail.com>
2022-04-25 00:45:53 -0300

    delete comments

--- src/main/java/com/astralTinderV1/controllers/HomeController.java
+++ src/main/java/com/astralTinderV1/controllers/HomeController.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.astralTinderV1.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
- *
- * @author sebag
- */
 @Controller
 @RequestMapping("/")
 public class HomeController {
