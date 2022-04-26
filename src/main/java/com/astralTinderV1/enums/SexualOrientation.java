882daa91f650d8fa7a7f0b17d662a618c5d4e2fc
Carlos Carmona <caneladelmoro@gmail.com>
2022-04-26 01:18:24 -0300

    Add atributo name a los enums gender, province y orSex, y medifico los select de user-register.html

--- src/main/java/com/astralTinderV1/enums/SexualOrientation.java
+++ src/main/java/com/astralTinderV1/enums/SexualOrientation.java
@@ -2,11 +2,21 @@ package com.astralTinderV1.enums;
 
 public enum SexualOrientation {
 
-    LESBIAN,
-    GAY,
-    HETEROSEXUAL,
-    BISEXUAL,
-    PANSEXUAL,
-    ASEXUAL,
-    OTHER;
+    LESBIAN("Lesbian"),
+    GAY("Gay"),
+    HETEROSEXUAL("Heterosexual"),
+    BISEXUAL("Bisexual"),
+    PANSEXUAL("Pansexual"),
+    ASEXUAL("Asexual"),
+    OTHER("Otro");
+    
+    private final String name;
+
+    private SexualOrientation(String name) {
+        this.name = name;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
 }
