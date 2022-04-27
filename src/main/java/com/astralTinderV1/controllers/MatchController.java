14428db13aa25f08e7c5dd569ae820dfe86a5628
Emiliana Goldsack <emigoldsack@gmail.com>
2022-04-27 19:20:40 -0300

    Match controller

--- src/main/java/com/astralTinderV1/controllers/MatchController.java
+++ src/main/java/com/astralTinderV1/controllers/MatchController.java
@@ -1,9 +1,11 @@
 package com.astralTinderV1.controllers;
 
+import com.astralTinderV1.enttities.User;
 import com.astralTinderV1.enttities.Vote;
 import com.astralTinderV1.services.ChooseYourPartnerService;
 import com.astralTinderV1.services.UserService;
 import com.astralTinderV1.services.VoteService;
+import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -28,23 +30,27 @@ public class MatchController {
     }
 
     @GetMapping()
-    public String ruleta(ModelMap model) {
+    public String ruleta(ModelMap model, HttpSession session) {
 
-        model.addAttribute("randomUser", cypS.randomUser());
-        model.addAttribute("vote", new Vote());
-        return "index/6ruletamuestrayacciones";
+//        User currentUser = (User) session.getAttribute("randomUser");
+//
+//        User random = cypS.randomUser();
+//        while (random.getId().equals(currentUser.getId())) {
+//            random = cypS.randomUser();
+//        }
+//        session.setAttribute("randomUser", cypS.randomUser());
+//
+//        model.addAttribute("vote", new Vote());
+        return "main-menu";
 
     }
-    
+
 //    @PostMapping
 //    public String addLike(@ModelAttribute Vote vote, ModelMap model){
 //        
 //        voteService.saveVote(vote);
+//        model.addAttribute("match", voteService.Match(vote));
 //        return null;
 //        
 //    }
-    
-    
-
-
 }
