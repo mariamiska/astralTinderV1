5a39d40af3727eefab3466f8eaf7ad0fa4afb58a
Carlos Carmona <caneladelmoro@gmail.com>
2022-04-30 23:11:38 -0300

    add enum type string a los atributosde  AstralPlane enttiti para porder vizualizar mejor los datos generados en db

--- src/main/java/com/astralTinderV1/enttities/AstralPlane.java
+++ src/main/java/com/astralTinderV1/enttities/AstralPlane.java
@@ -4,6 +4,8 @@ import com.astralTinderV1.enums.Cualidades;
 import com.astralTinderV1.enums.Elements;
 import com.astralTinderV1.enums.ZodiacSigns;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import lombok.Data;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -17,15 +19,20 @@ public class AstralPlane {
     @GeneratedValue(generator = "uuid")
     @GenericGenerator(name = "uuid", strategy = "uuid2")
     private String id;
-
+    
+    @Enumerated(EnumType.STRING)
     private ZodiacSigns ascendente;
-
+    
+    @Enumerated(EnumType.STRING)
     private ZodiacSigns lunarSign;
-
+    
+    @Enumerated(EnumType.STRING)
     private ZodiacSigns solarSign;
 
+    @Enumerated(EnumType.STRING)
     private Cualidades cualidad;
-
+    
+    @Enumerated(EnumType.STRING)
     private Elements element;
 
 }
