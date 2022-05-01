3ceb2758c6c5794b905741729f0352e702667267
Carlos Carmona <caneladelmoro@gmail.com>
2022-04-30 23:44:10 -0300

    refactor astral plane entiti

--- src/main/java/com/astralTinderV1/enttities/AstralPlane.java
+++ src/main/java/com/astralTinderV1/enttities/AstralPlane.java
@@ -4,8 +4,6 @@ import com.astralTinderV1.enums.Cualidades;
 import com.astralTinderV1.enums.Elements;
 import com.astralTinderV1.enums.ZodiacSigns;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import lombok.Data;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -20,19 +18,13 @@ public class AstralPlane {
     @GenericGenerator(name = "uuid", strategy = "uuid2")
     private String id;
     
-    @Enumerated(EnumType.STRING)
     private ZodiacSigns ascendente;
     
-    @Enumerated(EnumType.STRING)
     private ZodiacSigns lunarSign;
     
-    @Enumerated(EnumType.STRING)
     private ZodiacSigns solarSign;
 
-    @Enumerated(EnumType.STRING)
     private Cualidades cualidad;
     
-    @Enumerated(EnumType.STRING)
     private Elements element;
-
 }
