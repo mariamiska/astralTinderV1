fde71e3dacc5c5108eaf51eb865bc7f24205dc11
Carlos Carmona <caneladelmoro@gmail.com>
2022-05-01 18:45:04 -0300

    agrego atributos String info ya definidos para enum elements a fin de poder mostrarlos en perfil astral

--- src/main/java/com/astralTinderV1/enums/Cualidades.java
+++ src/main/java/com/astralTinderV1/enums/Cualidades.java
@@ -4,10 +4,10 @@ public enum Cualidades {
 
     CARDINAL("Cardinal",
             "Los signos de cualidad Cardinal son los que inician las estaciones (verano - otoño - invierno - primavera) por lo que estas personas son las que siempre dan el primer paso. Centran toda su energía en iniciar mil proyectos. Se asocia a ellos el poder, la acción y verse constantemente frente a nuevos retos. Se las ingenian, se impulsan y arriesgan porque hay coraje!",
-            "Emprendedores. Lideres. Voluntariosos. Desafiantes. Impetuosos"),
+            "Emprendedores. Lideres. Voluntariosos. Desafiantes. Impetuosos"),    
     FIJO("Fijo",
             "Los signos Fijos comienzan a mediaos de las estaciones (verano - otoño - invierno - primavera) por lo que no tieneden a ser iniciadores, pero si grandes ejecutores de ideas o proyectos. Con su propio empuje son determinados, persistentes, estables en sus acciones y confiables. Básicamente ellos deciden tomar el control de las cosas y ser responsables de que todo funcione. ",
-            "Estabilidad. Rigidez. Consistencia. Lealtad. Resistencia"),
+            "Estabilidad. Rigidez. Consistencia. Lealtad. Resistencia"),    
     MUTABLE("Mutable",
             "Los signos Fijos comienzan al final de las estaciones (verano - otoño - invierno - primavera) y por ello estan preparados con una flexibilidad organica para cambio. Son bastante receptivos a adaptarse de la mejor forma a los cambios que se presenten en la marcha. Presentan condiciones para evolucionar, salir de la zona de conford, generar conexiones y comunicar",
             "Flexibilidad. Versatilidad. Adaptabilidad. Inestabilidad. Impredecibles");
